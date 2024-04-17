b2079655af698db8bf57863d15fc399f4ba5c034
Launchable AI <contact@launchable.ai>
2024-04-17 12:24:36 -0400

    Remove download_file func; replace with video url

--- lambda_function.py
+++ lambda_function.py
@@ -23,18 +23,18 @@ def create_frame_output_dir(output_dir):
         shutil.rmtree(output_dir)  # Cleanup existing directory
         os.makedirs(output_dir)  # Recreate directory
 
-def extract_frame_from_video(video_file_path):
+def extract_frame_from_video(video_url):
     try:
-        print(f"Extracting from {video_file_path} at 5 frames per second with millisecond precision. This might take a bit...")
+        print(f"Extracting from {video_url} at 5 frames per second with millisecond precision. This might take a bit...")
         create_frame_output_dir(FRAME_EXTRACTION_DIRECTORY)
-        vidcap = cv2.VideoCapture(video_file_path)
+        vidcap = cv2.VideoCapture(video_url)
         fps = vidcap.get(cv2.CAP_PROP_FPS)
         print(f'Video FPS: {fps}')
 
         # Calculate the interval (in frame counts) at which to grab a frame to achieve 5 fps extraction.
         frame_interval = max(1, int(fps / 5))
         
-        output_file_prefix = os.path.basename(video_file_path).replace('.', '_')
+        output_file_prefix = os.path.basename(video_url).replace('.', '_')
         original_frame_number = 0
 
         while vidcap.isOpened():
@@ -62,21 +62,6 @@ def extract_frame_from_video(video_file_path):
     except Exception as e:
         print(f"Error extracting frames: {e}")
         return("Error extracting frames.")
-
-def download_file(url):
-    r = requests.get(url, stream=True)
-
-    # Extract filename from url
-    filename = url.split('/')[-1]
-
-    # Save the file to the /tmp directory
-    output_file_path = os.path.join("/tmp/", filename)
-
-    with open(output_file_path, 'wb') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                f.write(chunk)
-    return output_file_path
 
 class File:
     def __init__(self, file_path: str, display_name: str = None):
@@ -130,10 +115,7 @@ def handler(event, context):
     if not video_url:
         return 'Missing video_url in request body'
     
-    # download the file from the url
-    video_file_name = download_file(video_url)
-
-    parsing_result = extract_frame_from_video(video_file_name)
+    parsing_result = extract_frame_from_video(video_url)
 
     files_to_upload = prepare_files_to_upload()
 
